0749d7cb381099acf3043498c1e11f9fd9a4b238
champion <chengping@gdmbase.com>
2025-12-23 15:27:53 +0800

    [Feature]:增加limonp 修改描述： 兼容分析：无

---
+++ ports/limonp/portfile.cmake
@@ -0,0 +1,101 @@
+# Common Ambient Variables:
+#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
+#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
+#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
+#   CURRENT_INSTALLED_DIR     = ${VCPKG_ROOT_DIR}\installed\${TRIPLET}
+#   DOWNLOADS                 = ${VCPKG_ROOT_DIR}\downloads
+#   PORT                      = current port name (zlib, etc)
+#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
+#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
+#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
+#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
+#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
+#   VCPKG_TOOLCHAIN           = ON OFF
+#   TRIPLET_SYSTEM_ARCH       = arm x86 x64
+#   BUILD_ARCH                = "Win32" "x64" "ARM"
+#   DEBUG_CONFIG              = "Debug Static" "Debug Dll"
+#   RELEASE_CONFIG            = "Release Static"" "Release DLL"
+#   VCPKG_TARGET_IS_WINDOWS
+#   VCPKG_TARGET_IS_UWP
+#   VCPKG_TARGET_IS_LINUX
+#   VCPKG_TARGET_IS_OSX
+#   VCPKG_TARGET_IS_FREEBSD
+#   VCPKG_TARGET_IS_ANDROID
+#   VCPKG_TARGET_IS_MINGW
+#   VCPKG_TARGET_EXECUTABLE_SUFFIX
+#   VCPKG_TARGET_STATIC_LIBRARY_SUFFIX
+#   VCPKG_TARGET_SHARED_LIBRARY_SUFFIX
+#
+# 	See additional helpful variables in /docs/maintainers/vcpkg_common_definitions.md
+
+# Also consider vcpkg_from_* functions if you can; the generated code here is for any web accessable
+# source archive.
+#  vcpkg_from_github
+#  vcpkg_from_gitlab
+#  vcpkg_from_bitbucket
+#  vcpkg_from_sourceforge
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO yanyiwu/limonp
+    REF "v${VERSION}"
+    SHA512 3cf1103ea04610dce471c294b052e1bbef84a401c2a10d4251357423e302f4d3a2f3518c0ef332be009f1093f8a1b0bbfa7f857f7f09181ddb899246d59ff139
+    HEAD_REF master
+)
+
+# # Check if one or more features are a part of a package installation.
+# # See /docs/maintainers/vcpkg_check_features.md for more details
+# vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+#   FEATURES
+#     tbb   WITH_TBB
+#   INVERTED_FEATURES
+#     tbb   ROCKSDB_IGNORE_PACKAGE_TBB
+# )
+
+# 注释掉 测试相关的编译
+# 注释掉 ENABLE_TESTING()
+vcpkg_replace_string(
+    "${SOURCE_PATH}/CMakeLists.txt"
+    "ENABLE_TESTING()"
+    "# ENABLE_TESTING()"
+)
+
+# 注释掉 ADD_SUBDIRECTORY(test)
+vcpkg_replace_string(
+    "${SOURCE_PATH}/CMakeLists.txt"
+    "ADD_SUBDIRECTORY(test)"
+    "# ADD_SUBDIRECTORY(test)"
+)
+
+# 注释掉 ADD_TEST 命令
+vcpkg_replace_string(
+    "${SOURCE_PATH}/CMakeLists.txt"
+    "ADD_TEST(NAME ./test/demo COMMAND ./test/demo)"
+    "# ADD_TEST(NAME ./test/demo COMMAND ./test/demo)"
+)
+vcpkg_replace_string(
+    "${SOURCE_PATH}/CMakeLists.txt"
+    "ADD_TEST(NAME ./test/test.run COMMAND ./test/test.run)"
+    "# ADD_TEST(NAME ./test/test.run COMMAND ./test/test.run)"
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
+    # OPTIONS_RELEASE -DOPTIMIZE=1
+    # OPTIONS_DEBUG -DDEBUGGABLE=1
+)
+
+vcpkg_cmake_install()
+
+# # Moves all .cmake files from /debug/share/limonp/ to /share/limonp/
+# # See /docs/maintainers/ports/vcpkg-cmake-config/vcpkg_cmake_config_fixup.md for more details
+# When you uncomment "vcpkg_cmake_config_fixup()", you need to add the following to "dependencies" vcpkg.json:
+#{
+#    "name": "vcpkg-cmake-config",
+#    "host": true
+#}
+# vcpkg_cmake_config_fixup()
+
+# Uncomment the line below if necessary to install the license file for the port
+# as a file named `copyright` to the directory `${CURRENT_PACKAGES_DIR}/share/${PORT}`
+# vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
